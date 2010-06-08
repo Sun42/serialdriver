e9453a91cd283140394b0ec72fac0d00e63e35e0
sundas.christophe <sundas.christophe@dba7fb5b-969b-ccae-21d9-3c01c10cf0c1>
2010-06-08 15:35:25 +0000

    adding comments + docs

--- trunk/myserial.c
+++ trunk/myserial.c
@@ -3,16 +3,16 @@
 //-------------------------------------------------------------------
 #include "myserial.h"
 
-/**
- ** my_isr
+/** 
+ ** my_isr 
  ** @irq
- ** @devinfo
+ ** @devinfo 
  **/
 
 irqreturn_t my_isr(int irq, void *devinfo)
 {
   // static intrep = 0;
-  int	interrupt_id = inb(UART_INTR_ID) & 0x0F;
+  int	interrupt_id = inb(UART_IIR) & 0x0F;
   if (interrupt_id & 1)
     return IRQ_NONE;
 
@@ -22,17 +22,17 @@ irqreturn_t my_isr(int irq, void *devinfo)
   switch (interrupt_id)
     {
     case 6:	// Receiver Line Status (errors)
-      inb(UART_LINE_STAT);
+      inb(UART_LSR);
       break;
     case 4:	// Received Data Ready
     case 12:	// Character timeout
       wake_up_interruptible(&waitq_recv);
       break;
-    case 2:	// Transmitter Empty
+    case 2:	// Transmitter Empty 
       wake_up_interruptible(&waitq_xmit);
       break;
     case 0:	// Modem Status Changed
-      inb(UART_MODEM_STAT);
+      inb(UART_MSR);
       break;
     }
   return IRQ_HANDLED;
@@ -43,14 +43,13 @@ irqreturn_t my_isr(int irq, void *devinfo)
  ** au cas ou l'on voudrait `unload` le driver par exemple
  **/
 
-
-int	my_proc_read(char *buf, char **start, off_t off, int count, int *eof, void *data)
+int	my_proc_read(char *buf, char **start, off_t off, int count, int *eof, void *data) 
 {
-  int interrupt_id = inb(UART_INTR_ID);
-  int line_status = inb(UART_LINE_STAT);
-  int modem_status = inb(UART_MODEM_STAT);
+  int interrupt_id = inb(UART_IIR);
+  int line_status = inb(UART_LSR);
+  int modem_status = inb(UART_MSR);
   int len = 0;
-
+  
   len += sprintf(buf + len, "\n %02X=modem_status  ", modem_status);
   len += sprintf(buf + len, "\n %02X=line_status   ", line_status);
   len += sprintf(buf + len, "\n %02X=interrupt_id  ", interrupt_id);
@@ -66,34 +65,54 @@ int	my_proc_read(char *buf, char **start, off_t off, int count, int *eof, void *
 static int __init	uart_init(void)
 {
   printk(KERN_INFO "\nInstalling \'%s\' module\n", modname);
-
+  
 
   //initialisation de files d'attentes emission/reception
   init_waitqueue_head(&waitq_xmit);
   init_waitqueue_head(&waitq_recv);
 
-  // configure l' UART
-  outb(0x00, UART_INTR_EN);
-  outb(0xC7, UART_FIFO_CTRL);
-  outb(0x83, UART_LINE_CTRL);
+  /**
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~ configure l' UART
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  **/
+  // on ne veut pas d'interruptions pour le moment
+  outb(0x00, UART_IER);
+
+  /*
+  ** Chris:  faut-il prendre en compte l'endianess (little)?, le 14 bytes me parait louche, le reste me parait logique
+  ** 0xC7 <=> 1100 0111
+  ** Bit 0  : [1]	=> Enable FIFO
+  ** Bit 1  : [1]	=> Clear receive FIFO
+  ** Bit 2  : [1]	=> Clear transmit FIFO
+  ** Bit 3  : [0]	=> DMA mode 0
+  ** Bit 4  : [0]	=> Reserved
+  ** Bit 5  : [0]	=> NO 64 bit
+  ** Bit 6/7: [1][1]	=> interupt every 14 bytes 
+   */
+  outb(0xC7, UART_FCR);
+
+  /
+  outb(0x83, UART_LCR);
   outb(0x01, UART_DLATCH_LO);
   outb(0x00, UART_DLATCH_HI);
-  outb(0x03, UART_LINE_CTRL);
-  outb(0x03, UART_MODEM_CTRL);
-  inb(UART_MODEM_STAT);
-  inb(UART_LINE_STAT);
+  outb(0x03, UART_LCR);
+  outb(0x03, UART_MCR);
+  inb(UART_MSR);
+  inb(UART_LSR);
   inb(UART_RX_DATA);
-  inb(UART_INTR_ID);
-
-  // demane d'irq
+  inb(UART_IIR);
+  
+  // demande d'irq
   if (request_irq(UART_IRQ, my_isr, IRQF_SHARED, modname, &modname) < 0)
     {
       printk(KERN_WARNING, "%s couldn't get IRQ %i", module, UART_IRQ);
       return -EBUSY;
     }
-  outb(INTR_MASK, UART_INTR_EN);
-  outb(0x0B, UART_MODEM_CTRL);
-  printk(KERN_INFO " Interrupt-ID: %02X \n", inb(UART_INTR_ID));
+  outb(INTR_MASK, UART_IER);
+  // ??
+  outb(0x0B, UART_MCR);
+  printk(KERN_INFO " Interrupt-ID: %02X \n", inb(UART_IIR));
 
   create_proc_read_entry(modname, 0, NULL, my_proc_read, NULL);
   return register_chrdev(my_major, modname, &my_fops);
@@ -105,8 +124,10 @@ static void __exit	uart_exit(void)
   unregister_chrdev(my_major, modname);
   remove_proc_entry(modname, NULL);
 
-  outb( 0x00, UART_INTR_EN);
-  outb( 0x00, UART_MODEM_CTRL);
+  //
+  outb(0x00, UART_IER);
+  //Set to `Data terminal ready`
+  outb(0x00, UART_MCR);
   free_irq(UART_IRQ, modname);
 
   printk(KERN_INFO "<1>Removing \'%s\' module\n", modname);
@@ -114,20 +135,20 @@ static void __exit	uart_exit(void)
 
 module_init(uart_init);
 module_exit(uart_exit);
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL"); 
 
 ssize_t my_read(struct file *file, char *buf, size_t len, loff_t *pos)
 {
-  int count, i, line_status = inb(UART_LINE_STAT);
-
+  int count, i, line_status = inb(UART_LSR);
+  
   if ((line_status & 1) == 0)
     {
       if (file->f_flags & O_NONBLOCK)
 	return 0;
-      if (wait_event_interruptible(waitq_recv,(inb( UART_LINE_STAT) & 1)))
+      if (wait_event_interruptible(waitq_recv,(inb( UART_LSR) & 1)))
 	return -EINTR;
     }
-
+  
   count = 0;
   for (i = 0; i < len; i++)
     {
@@ -135,7 +156,7 @@ ssize_t my_read(struct file *file, char *buf, size_t len, loff_t *pos)
       if (copy_to_user(buf + i, &datum, 1))
 	return -EFAULT;
       ++count;
-      if ((inb(UART_LINE_STAT) & 1) == 0)
+      if ((inb(UART_LSR) & 1) == 0)
 	break;
     }
   return count;
@@ -144,14 +165,14 @@ ssize_t my_read(struct file *file, char *buf, size_t len, loff_t *pos)
 ssize_t my_write(struct file *file, const char *buf, size_t len, loff_t *pos)
 {
   int	i, count = 0;
-  int   modem_status = inb(UART_MODEM_STAT);
+  int   modem_status = inb(UART_MSR);
 
   if ((modem_status & 0x10) != 0x10)
     {
       if (file->f_flags & O_NONBLOCK)
 	return 0;
-      if (wait_event_interruptible(waitq_xmit, (inb(UART_MODEM_STAT)& 0x10) == 0x10))
-	return -EINTR;
+      if (wait_event_interruptible(waitq_xmit, (inb(UART_MSR)& 0x10) == 0x10))
+	return -EINTR; 
     }
 
   for (i = 0; i < len; i++)
@@ -159,11 +180,11 @@ ssize_t my_write(struct file *file, const char *buf, size_t len, loff_t *pos)
       unsigned char datum;
       if (copy_from_user(&datum, buf + i, 1))
 	return -EFAULT;
-      while ((inb(UART_LINE_STAT) & 0x20) == 0);
+      while ((inb(UART_LSR) & 0x20) == 0); 
       outb(datum, UART_TX_DATA);
       ++count;
-      if ((inb(UART_MODEM_STAT) & 0x10) != 0x10)
-	break;
+      if ((inb(UART_MSR) & 0x10) != 0x10)
+	break; 
     }
   return count;
 }
@@ -176,7 +197,7 @@ unsigned int my_poll(struct file *file, struct poll_table_struct *wait)
 {
   unsigned int mask = 0;
 
-  /**
+  /** 
    ** Met en file d'attente le processus courant dans toutes les files d'attentes
    ** susceptibles de le reveiller par la suite (poll_wait)
    ** poll_wait: http://www.makelinux.net/ldd3/chp-6-sect-3.shtml
@@ -185,10 +206,10 @@ unsigned int my_poll(struct file *file, struct poll_table_struct *wait)
   poll_wait(file, &waitq_xmit, wait);
 
   // si read() est ready pour retourner de la data
-  if (inb(UART_LINE_STAT) & 0x01)
+  if (inb(UART_LSR) & 0x01)
     mask |= (POLLIN | POLLRDNORM);
   // si write() est ready pour accepter de la data
-  if (inb(UART_LINE_STAT) & 0x20)
+  if (inb(UART_LSR) & 0x20)
     mask |= (POLLOUT | POLLWRNORM);
   return mask;
 }
--- trunk/myserial.h
+++ trunk/myserial.h
@@ -22,18 +22,18 @@ int		my_proc_read(char *buf, char **start, off_t off, int count, int *eof, void 
 #define INTR_MASK	0x0F	// UART Interrupt Mask
 
 enum {
-  UART_RX_DATA = UART_BASE + 0,
-  UART_TX_DATA = UART_BASE + 0,
-  UART_DLATCH_LO = UART_BASE + 0,
-  UART_DLATCH_HI = UART_BASE + 1,
-  UART_INTR_EN = UART_BASE + 1,
-  UART_INTR_ID = UART_BASE + 2,
-  UART_FIFO_CTRL = UART_BASE + 2,
-  UART_LINE_CTRL = UART_BASE + 3,
-  UART_MODEM_CTRL = UART_BASE + 4,
-  UART_LINE_STAT = UART_BASE + 5,
-  UART_MODEM_STAT = UART_BASE + 6,
-  UART_SCRATCH = UART_BASE + 7,
+  UART_RX_DATA = UART_BASE + 0,		//
+  UART_TX_DATA = UART_BASE + 0,		//
+  UART_DLATCH_LO = UART_BASE + 0,	// DLL divisor latch lsb
+  UART_DLATCH_HI = UART_BASE + 1,	// DLM divisor latch msb
+  UART_IER = UART_BASE + 1,		// Interrupt enable register
+  UART_IIR = UART_BASE + 2,		// Interrupt identification register
+  UART_FCR = UART_BASE + 2,		// Fifo Control Register
+  UART_LCR = UART_BASE + 3,		// Line Control Register
+  UART_MCR = UART_BASE + 4,		// Modem Control  Register
+  UART_LSR = UART_BASE + 5,		// line status Register
+  UART_MSR = UART_BASE + 6,		// Modem Status Register
+  UART_SCR = UART_BASE + 7,		// Scratch
 };
 
 char modname[] = "myserial";

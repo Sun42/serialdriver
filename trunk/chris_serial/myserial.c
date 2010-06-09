944cffac9463fe10a9a0a0b649679fb8f934f97b
sundas.christophe <sundas.christophe@dba7fb5b-969b-ccae-21d9-3c01c10cf0c1>
2010-06-09 15:44:51 +0000

    blah

--- trunk/chris_serial/myserial.c
+++ trunk/chris_serial/myserial.c
@@ -1,16 +1,14 @@
 #include "myserial.h"
 
-#define DRIVER_VERSION "0.1"
-#define DRIVER_AUTHOR "Chris"
-#define DRIVER_DESC "Super Serial Driver"
 
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL");
 
-static	int __init myserial_init(void)
+static	int	__init myserial_init(void)
 {
   printk(KERN_INFO "Loading Driver %s version %s", DRIVER_DESC, DRIVER_VERSION);
+  
   return (0);
 }
 
@@ -18,6 +16,94 @@ static	int __init myserial_init(void)
 static void	__exit myserial_exit(void)
 {
   printk(KERN_INFO "Unloading Driver %s version %s", DRIVER_DESC, DRIVER_VERSION);
+}
+
+// Mandatories
+
+/*
+ * Return string describing the specified port
+ */
+static const char *myserial_type(struct uart_port *port)
+{
+  return (port->type == MYSERIAL_PORT ? "myserial_port" : NULL);
+}
+
+
+
+/*
+ * Start transmitting.
+ */
+static void myserial_start_tx(struct uart_port *port)
+{
+
+}
+
+/*                                                                                                                                           * Stop transmitting.                                                                                                                        */
+static void myserial_stop_tx(struct uart_port *port)
+{
+}
+
+/*
+** Stop transmitting
+*/
+static void myserial_stop_rx(struct uart_port *port)
+{
+
+}
+
+static int myserial_startup(struct uart_port *port)
+{
+  return 0;
+}
+
+/*
+ * Disable the port
+ */
+static void myserial_shutdown(struct uart_port *port)
+{
+}
+
+//Optionals
+static unsigned int	myserial_tx_empty(struct uart_port *port) 
+{
+  return 0;
+}
+static void		myserial_set_mctrl(struct uart_port *port, unsigned int mctrl) 
+{ 
+}
+
+static unsigned int	myserial_get_mctrl(struct uart_port *port) 
+{ 
+  return 0;
+}
+
+static void		myserial_break_ctl(struct uart_port *port, int break_state) 
+{ 
+
+}
+
+static void		myserial_enable_ms(struct uart_port *port) 
+{ 
+}
+static void		myserial_release_port(struct uart_port *port) 
+{ 
+}
+
+static int		myserial_request_port(struct uart_port *port) 
+{ 
+  return 0;
+}
+static void		myserial_config_port(struct uart_port *port, int flags) 
+{
+}
+
+static int		myserial_verify_port(struct uart_port *port, struct serial_struct *ser) 
+{ 
+  return 0; 
+}
+
+static void		myserial_set_termios(struct uart_port *port, struct ktermios *new, struct ktermios *old) 
+{
 }
 
 module_init(myserial_init);
--- trunk/chris_serial/myserial.h
+++ trunk/chris_serial/myserial.h
@@ -7,20 +7,71 @@
 #include <linux/module.h>		
 
 
-/* #include <linux/console.h> */
-/* #include <linux/tty.h> */
-/* #include <linux/tty_flip.h> */
-/* #include <linux/serial.h> */
+#include <linux/console.h> 
+#include <linux/tty.h>
+#include <linux/tty_flip.h>
+#include <linux/serial.h> 
 #include <linux/serial_core.h>		// high level serial API
 
-struct uart_driver	myuart_driver = {
-    .owner        = THIS_MODULE,
-    .driver_name  = FOO_SERIAL_NAME,
-    .dev_name     = "ttyS",
-    .major        = TTY_MAJOR,
-    .minor        = 64,
-    .nr           = FOO_SERIAL_NR,
-  };
+# define MYSERIAL_PORT     4
+
+#define DRIVER_VERSION "0.1"
+#define DRIVER_AUTHOR "Chris"
+#define DRIVER_DESC "Super Serial Driver"
+
+static void myserial_stop_tx(struct uart_port *port);
+static void myserial_start_tx(struct uart_port *port);
+static void myserial_stop_rx(struct uart_port *port);
+static void		myserial_enable_ms(struct uart_port *port);
+static int myserial_startup(struct uart_port *port);
+static void myserial_shutdown(struct uart_port *port);
+static unsigned int myserial_tx_empty(struct uart_port *port);
+static void myserial_set_mctrl(struct uart_port *port, unsigned int mctrl);
+static unsigned int	myserial_get_mctrl(struct uart_port *port);
+static void		myserial_break_ctl(struct uart_port *port, int break_state);
+static void		myserial_enable_ms(struct uart_port *port) ;
+static void		myserial_release_port(struct uart_port *port) ; 
+static const char *myserial_type(struct uart_port *port);
+static int		myserial_request_port(struct uart_port *port) ;
+static void		myserial_config_port(struct uart_port *port, int flags) ;
+static int		myserial_verify_port(struct uart_port *port, struct serial_struct *ser) ;
+static void		myserial_set_termios(struct uart_port *port, struct ktermios *new, struct ktermios *old) ;
+
+
+static struct uart_ops myserial_ops = {
+  .tx_empty     = myserial_tx_empty,
+  .set_mctrl    = myserial_set_mctrl,
+  .get_mctrl    = myserial_get_mctrl,
+  .stop_tx      = myserial_stop_tx,
+  .start_tx     = myserial_start_tx,
+  .stop_rx      = myserial_stop_rx,
+  .enable_ms    = myserial_enable_ms,
+  .break_ctl    = myserial_break_ctl,
+  .startup      = myserial_startup,
+  .shutdown     = myserial_shutdown,
+  .type         = myserial_type,
+  .release_port = myserial_release_port,
+  .request_port = myserial_request_port,
+  .config_port  = myserial_config_port,
+  .verify_port  = myserial_verify_port,
+  .set_termios  = myserial_set_termios,
+};
+
+
+
+
+
+
+
+
+/* struct uart_driver	myuart_driver = { */
+/*     .owner        = THIS_MODULE, */
+/*     .driver_name  = FOO_SERIAL_NAME, */
+/*     .dev_name     = "ttyS", */
+/*     .major        = TTY_MAJOR, */
+/*     .minor        = 64, */
+/*     .nr           = FOO_SERIAL_NR, */
+/*   }; */
 /*
 372 struct uart_driver {
 373         struct module           *owner;
